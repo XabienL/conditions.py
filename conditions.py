2862ee7b4b748e4200353c2ee6739fd70124901c
XabienL <98927144+XabienL@users.noreply.github.com>
2022-02-15 21:17:50 -0500

    Add files via upload
    condition.py

---
+++ conditions.py
@@ -0,0 +1,11 @@
+temp = int(input("Enter temp: "))
+if temp > 90:
+    print("Wear shorts")
+if temp > 70:
+    print("Short sleeves are fine")
+if temp > 50:
+    print("Wear a jacket")
+if temp > 32:
+    print("Wear a heavy coat")
+else:
+    print("Stay Inside")
